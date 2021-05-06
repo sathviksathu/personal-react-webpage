28394719ba42aa22d365aaefb283940a57fd1688
sathviksathu <sathu.sathvik@gmail.com>
2021-05-06 22:03:01 +0530

    Update footer with appropriate medium link

--- src/footer.js
+++ src/footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component{
      <ul className="footer" >
      <li><a href= "https://github.com/sathviksathu" target="_blank"><FaGithub /> Github </a></li>
         <li><a href="https://www.linkedin.com/in/sathvik-sanagavarapu-77330a98/" target="_blank"> <FaLinkedin /> LinkedIn </a></li>
-        <li><a href="https://medium.com/@sathu.sathvik" target="_blank"> <FaMedium /> Medium </a></li>
+        <li><a href="https://sathu-sathvik.medium.com/" target="_blank"> <FaMedium /> Medium </a></li>
         <li><a href="https://sathvik-resume.s3.ap-south-1.amazonaws.com/Resume.pdf" target="_blank"><FaFilePdf /> Resume </a></li>
     </ul>
 </div>
